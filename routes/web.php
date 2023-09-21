1636b179e3d079c9ac3768d337857502e6d279ea
mralikeshtkar <alikeshtkar262@gmail.com>
2023-09-22 00:31:43 +0330

    123

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Utility;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\RegisteredUserController;
