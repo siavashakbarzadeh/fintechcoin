fcef9d4f5983fc8ced4fd4a8f3ebf11e5ab31dd1
mralikeshtkar <alikeshtkar262@gmail.com>
2023-09-21 23:36:43 +0330

    123

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +16,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+
+            // your schedule code
+            Log::info('Working');
+
+        })->everyMinute();
     }
 
     /**
