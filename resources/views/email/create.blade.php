96fb43ec4416d0c0d9b50b7d87574f70c1c3aa99
mralikeshtkar <alikeshtkar262@gmail.com>
2023-09-21 19:11:28 +0330

    123

--- resources/views/email/create.blade.php
+++ resources/views/email/create.blade.php
@@ -58,19 +58,10 @@
 									<div class="col-md-4 mb-2">
 										<div class="mb-3">
                                             <div class="input-group input-group-merge">
-                                                <select multiple="multiple" class="multiple-select">
-                                                    <option value="1">January</option>
-                                                    <option value="2">February</option>
-                                                    <option value="3">March</option>
-                                                    <option value="4">April</option>
-                                                    <option value="5">May</option>
-                                                    <option value="6">June</option>
-                                                    <option value="7">July</option>
-                                                    <option value="8">August</option>
-                                                    <option value="9">September</option>
-                                                    <option value="10">October</option>
-                                                    <option value="11">November</option>
-                                                    <option value="12">December</option>
+                                                <select name="emails[]" multiple="multiple" class="multiple-select">
+                                                    @foreach($customers as $customer)
+                                                        <option value="{{ $customer->email }}">{{ $customer->name }}</option>
+                                                    @endforeach
                                                 </select>
                                             </div>
 										</div>
